348822381db1eb49ca2956aae822689e18195985
marshall setian <marshallsetian@gmail.com>
2022-11-07 20:40:49 +0700

    Update spam.py

--- spam.py
+++ spam.py
@@ -1,17 +1,54 @@
-import os, sys, time, requests as r, time, json, base64
-from fake_useragent import UserAgent
-from time import sleep as s
+mport os,sys,time,os,json,requests,json
+from colorama import Fore,Back,init
+from requests import get,post
+from urllib import request
 
-k= '\033[1;33m'; a = '\033[1;30m'; m= '\033[1;31m'; h = '\033[1;32m'; p = '\033[1;37m';
-ua = UserAgent().random
-ses = r.Session()
-banner = f'''
-{m} ___ {p} ____   __    __  __  __  __  ____  ____ {m}  ____  ____
-{m}/ __){p}(  _ \ /__\  (  \/  )(  \/  )( ___)(  _ \ {m}(_  _)(  _ \\
-{m}\__ \{p} )___//(__)\  )    (  )    (  )__)  )   /  {m}_)(_  )(_) )
-{m}(___/{p}(__) (__)(__)(_/\/\_)(_/\/\_)(____)(_)\_){m} (____)(____/
-{m} [{k} /{m} ]{p} Author :{k} marshall_setian
-{m} [{k} \{m} ]{p} Github :{a} https://github.com/marshallsetian
+def autoketik(s):
+    for c in s + "\n":
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(0.008)
+
+B = Fore.BLUE
+W = Fore.WHITE
+R = Fore.RED
+G = Fore.GREEN
+BL = Fore.BLACK
+Y = Fore.YELLOW
+
+ip=requests.get('https://api.ipify.org').text
+visitor=request.urlopen('https://api.countapi.xyz/hit/brutal-spam-wa')
+getvisit=json.loads(visitor.read())
+localtime=time.asctime(time.localtime(time.time()))
+
+hijau="\033[1;92m "
+putih="\033[1;97m"
+abu="\033[1;90m"
+kuning="\033[1;93m"
+ungu="\033[1;95m"
+merah="\033[1;91m"
+biru="\033[1;96m"
+
+os.system("clear")
+autoketik(f"{biru}[{kuning}Warning{biru}] {W}Mari berteman... :) @Marshall Setian")
+time.sleep(3)
+os.system("xdg-open https://www.facebook.com/yoelmars1509")
+autoketik(f"{biru}[{kuning}Warning{biru}] {W}Thx yang udah FOLLOW YAH.., semoga work")
+time.sleep(3)
+os.system("clear")
+autoketik(f"""
+{hijau}╔═╗{merah}┌─┐┌─┐┌┬┐  {biru}╦ ╦┬ ┬┌─┐┌┬┐{putih}┌─┐{kuning}┌─┐┌─┐ ┌─┐
+{hijau}╚═╗{merah}├─┘├─┤│││  {biru}║║║├─┤├─┤ │ {putih}└─┐{kuning}├─┤├─┘ ├─┘
+{hijau}╚═╝{merah}┴  ┴ ┴┴ ┴  {biru}╚╩╝┴ ┴┴ ┴ ┴ {putih}└─┘{kuning}┴ ┴┴   ┴ {ungu}V1
+{abu}-----------------------------------------
+{putih}[{biru}•{putih}] {biru}Author {putih}   : MarshallSetian
+{putih}[{biru}•{putih}] {abu}GitHub {putih}   : marshallsetian
+{putih}[{biru}•{putih}] {merah}You{putih}Tube {putih}  : marshall setian
+{putih}[{biru}•{putih}] {ungu}Instagram {putih}: @marshall_setian
+{W}[{Y}•{W}] Ip Kamu {putih}  :{Y} {ip}
+{W}[{Y}•{W}] Waktu/Jam {putih}:{Y} {localtime}
+{W}[{Y}•{W}] Total Run {putih}:{Y} {getvisit['value']}
+""")
 '''
 def wangsaf():
 	os.system('clear'); print(banner)
